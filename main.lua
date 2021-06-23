e7c06236467850d2ade4c34cd0835e33c7183dcc
Scott Harrison <scottrules44@gmail.com>
2021-06-22 20:36:57 -0500

    1.0
    1.0

---
+++ main.lua
@@ -0,0 +1,45 @@
+local firebaseDynamicLinks = require "plugin.firebaseDynamicLinks"
+local json = require "json"
+local widget = require "widget"
+firebaseDynamicLinks.init()
+
+local longLink =""
+
+local background = display.newRect( display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
+background.alpha = .95
+background:setFillColor(.5,0,0)
+
+local title = display.newText( "Firebase Dynamic Links", display.contentCenterX, display.contentCenterY-180 , native.systemFont, 18 )
+title:setFillColor( 0 )
+
+local createLink = widget.newButton( {
+    label = "Create Link",
+    id = "createLink",
+    onRelease = function ( )
+        print("run1312")
+        longLink = firebaseDynamicLinks.buildUrl({domain="e2rfu.app.goo.gl", iOSBundleID="com.mycompany.App", iOSCustomScheme="firebaseTest",iOSAppStoreID="284882215",iOSFallbackURL="https://facebook.com", androidPackageName="com.mycompany.App",androidFallbackURL="https://google.com", analyticsMedium="test medium", analyticsSource="test app", analyticsCampaign="test campagin", analyticsTerm="term" })
+        print(longLink)
+        print("run1315")
+    end
+} )
+createLink.x, createLink.y = display.contentCenterX, display.contentCenterY-20
+createLink:setFillColor( 0 )
+
+local shortenLink = widget.newButton( {
+    label = "Shorten Link",
+    id = "shortenLink",
+    onRelease = function ( )
+      firebaseDynamicLinks.shortenURL(longLink,function(e)
+        if e.isError == false then
+          print(e.url)
+        end
+      end)
+    end
+} )
+shortenLink.x, shortenLink.y = display.contentCenterX, display.contentCenterY+20
+shortenLink:setFillColor( 0 )
+
+
+Runtime:addEventListener("firebaseDynamicLinks", function(e)
+  print(json.encode(e))
+end)
